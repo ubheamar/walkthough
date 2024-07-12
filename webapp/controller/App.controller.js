81e9d422fbf97f24760424a8a570626628f585df
Amar Ubhe <amar.ubhe@sap.com>
2024-07-12 19:38:43 +0530

    Session Walkthrough Step 7

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,28 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
- ], (Controller,MessageToast) => {
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+ ], (Controller,MessageToast,JSONModel) => {
     "use strict";
  
     return Controller.extend("ui5.walkthrough.controller.App", {
+       onInit : function(){
+           // set data model on view
+
+         const oDummyData = {
+            recipient : {
+               name : "World"
+            },
+         };
+         var oModel = new JSONModel(oDummyData);
+         this.getView().setModel(oModel,"abcModel");
+       },
+       onLiveChange : function(oEvent){
+          
+       },
        onShowHello() {
           MessageToast.show("Hello World")
+          
        }
     });
  });
